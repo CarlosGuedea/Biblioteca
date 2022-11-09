66b41cf167a1d4f0dd96c161155fe2f116663181
CarlosGuedea <57150568+CarlosGuedea@users.noreply.github.com>
2022-11-09 12:07:34 -0600

    index2

--- index.php
+++ index.php
@@ -24,9 +24,32 @@ if(isset($_POST['No_cuenta'])){
     }
 }
 
-include 'templates/header.php';
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="public/css/normalize.css">
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
+    <div class="container">
+        <div class="row">
+            <div class="col-1">
+                <picture>
+                    <img class="img-fluid w-10 h-10" width="80px" src="public/img/logo.jpg" alt="biblioteca">
+                </picture>
+            </div>
+            <div class="col">
+                <h1 class="text-center Header">Biblioteca Servicios en la Nube</h1>
+            </div>
+        </div>
+    </div>
 
 <body>
     <div class="container contenedor">
@@ -74,7 +97,7 @@ include 'templates/header.php';
         </div>
     </div>
     <?php
-    include 'templates/footer.php'
+    include 'template/footer.php'
     ?>
 </body>
 </html>
